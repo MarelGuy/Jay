d6e3a1b1edaaa501c5d6c6e98465fa979d8d9837
MarelGuy <golans.gaming@gmail.com>
2022-01-24 10:24:30 +0100

    testable main

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ mod token;
 
 fn main() {
     let args: Vec<String> = args().collect();
+    let filename = &args[1];
 
-    let file_content = fs::read_to_string(&args[1]).unwrap();
+    let file_content = fs::read_to_string(&filename).unwrap();
+    let mut actual_lexer: lexer::Lexer = lexer::Lexer::new(file_content);
+    print!("{:?}", actual_lexer.make_tokens());
 }
