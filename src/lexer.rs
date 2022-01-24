5bd3964ebd805b73e1ca225b41436949ca24cbae
MarelGuy <golans.gaming@gmail.com>
2022-01-24 10:24:06 +0100

    fixed previous bug and added the Debug trait

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,7 @@
 use crate::token::Token;
+use std::fmt::Debug;
+use std::fmt::Error;
+use std::fmt::Formatter;
 
 pub struct Lexer {
     text: String,
@@ -6,18 +9,26 @@ pub struct Lexer {
     current_char: char,
 }
 
-impl Lexer {
-    pub fn new(self, text: String) -> Lexer {
-        self.advance();
+impl Debug for Lexer {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(
+            f,
+            "Lexer {{ text: {}, position: {}, current_char: {} }}",
+            self.text, self.position, self.current_char
+        )
+    }
+}
 
+impl Lexer {
+    pub fn new(text: String) -> Lexer {
         Lexer {
             text: text,
-            position: -1,
+            position: 0,
             current_char: '\0',
         }
     }
 
-    pub fn advance(mut self) {
+    pub fn advance(&mut self) {
         self.position += 1;
 
         if self.position > self.text.len() as isize {
@@ -31,55 +42,52 @@ impl Lexer {
         }
     }
 
-    pub fn make_number(self) -> Token {
-        let num_str = "";
-        let dot_count = 0;
+    pub fn make_number(&self, current_string: &str) -> Token {
+        let mut dot_count = 0;
 
-        while assert_eq!(self.current_char, '\0') && self.current_char.is_numeric() + '.' {
-            if self.current_char == '.' {
-                if dot_count == 1 {
-                    break;
-                } else {
-                    dot_count += 1;
-                    num_str = &(num_str.to_owned() + ".");
-                }
-            } else {
-                num_str = &self.current_char.to_string();
+        for elem in current_string.chars() {
+            if elem == '.' {
+                dot_count += 1;
             }
-
-            num_str = &self.current_char.to_string();
         }
 
-        let finished_token = Token::new("TT_FLOAT".to_owned(), num_str.to_string());
-
-        return finished_token;
+        if dot_count > 1 {
+            return Token::new("TT_FLOAT".to_owned(), current_string.to_string());
+        } else {
+            return Token::new("TT_INT".to_owned(), current_string.to_string());
+        }
     }
+    pub fn make_tokens(&mut self) -> Vec<Token> {
+        self.advance();
+        let mut tokens: Vec<Token> = Vec::new();
 
-    pub fn make_tokens(self) -> Vec<Token> {
-        let mut tokens: Vec<Token> = vec![];
+        while self.current_char != '\0' {
+            print!("{}", self.current_char);
 
-        while assert_eq!(self.current_char, '\0') {
             if self.current_char.is_whitespace() {
                 self.advance();
             } else if self.current_char.is_numeric() {
-                tokens.push(self.make_number());
-            } else if assert_eq!(self.current_char, '+') {
+                tokens.push(self.make_number(&self.text as &str)); // The problem is here
+            } else if self.current_char == '+' {
                 tokens.push(Token::new("TT_PLUS".to_owned(), "+".to_owned()));
                 self.advance();
-            } else if assert_eq!(self.current_char, '-') {
+            } else if self.current_char == '-' {
                 tokens.push(Token::new("TT_MINUS".to_owned(), "-".to_owned()));
                 self.advance();
-            } else if assert_eq!(self.current_char, '/') {
+            } else if self.current_char == '/' {
                 tokens.push(Token::new("TT_DIV".to_owned(), "/".to_owned()));
                 self.advance();
-            } else if assert_eq!(self.current_char, '*') {
+            } else if self.current_char == '*' {
                 tokens.push(Token::new("TT_MUL".to_owned(), "*".to_owned()));
                 self.advance();
-            } else if assert_eq!(self.current_char, '(') {
+            } else if self.current_char == '(' {
                 tokens.push(Token::new("TT_LPAREN".to_owned(), "(".to_owned()));
                 self.advance();
-            } else if assert_eq!(self.current_char, ')') {
+            } else if self.current_char == ')' {
                 tokens.push(Token::new("TT_RPAREN".to_owned(), ")".to_owned()));
+                self.advance();
+            } else {
+                tokens.push(Token::new("\0".to_owned(), "\0".to_owned()));
                 self.advance();
             }
         }
