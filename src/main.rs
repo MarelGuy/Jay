144d0cf189193e378d87872db64cca1936b2efa4
MarelGuy <golans.gaming@gmail.com>
2022-03-03 19:35:20 +0100

    Removed file IO. Added a basic interpreter.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,23 @@
-use std::env::args;
-use std::ffi::OsStr;
-use std::fs;
-use std::path::Path;
+use std::io::Write;
+
+use chrono::{Datelike, Utc};
 
 mod lexer;
 mod token;
 
 fn main() {
-    let args: Vec<String> = args().collect();
+    println!("Jay version 0.0.0 (c) {}", Utc::now().date().year());
 
-    if args.len() < 2 || args.len() > 2 {
-        println!("Usage: {} <file>", args[0]);
-        return;
-    }
+    loop {
+        print!(">>> ");
+        std::io::stdout().flush().expect("Could not flush stdout");
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
 
-    let file_path: &Path = Path::new(&args[1]);
+        let mut lexer = lexer::Lexer::new(input);
 
-    if !file_path.exists() {
-        println!("File not found: {}", args[1]);
-        return;
-    }
-
-    if file_path.extension().and_then(OsStr::to_str) != Some("jay") {
-        println!("File extension must be .jay: {}", args[1]);
-        return;
-    }
-
-    let mut lexer: lexer::Lexer = lexer::Lexer::new(fs::read_to_string(&args[1]).unwrap());
-
-    for elem in lexer.make_tokens() {
-        print!("{:}", elem.get_token());
+        println!("{:?}", lexer.next_token().unwrap());
     }
 }
