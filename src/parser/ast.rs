fae2a70578a9a3ebbe6e4d0fd4e1c75033ee1589
Marelguy <golans.gaming@gmail.com>
2022-04-29 12:27:28 +0200

    Added basic math nodes, i may add a derive clone

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -5,17 +5,54 @@
 
 use crate::lexer::token::Token;
 
+pub struct Node<T> {
+    children: Vec<T>,
+    node: T,
+}
+
+impl<T> Node<T> {
+    pub fn new(children: Vec<T>, node: T) -> Self {
+        Self { children, node }
+    }
+}
+
 pub struct NumberNode<'a> {
-    pub token: Token<'a>,
+    token: Token<'a>,
 }
 
-pub struct Node<'a> {
-    token: Token<'a>,
-    children: Vec<Node<'a>>,
+impl<'a> NumberNode<'a> {
+    pub fn new(token: Token<'a>) -> Self {
+        Self { token }
+    }
 }
 
-pub struct BinOpNode<'a, T> {
-    left: Box<T>,
-    token: Token<'a>,
-    right: Box<T>,
+pub struct BinOpNode<'a> {
+    left_node: Node<NumberNode<'a>>,
+    op_token: Token<'a>,
+    right_node: Node<NumberNode<'a>>,
+}
+
+impl<'a> BinOpNode<'a> {
+    pub fn new(
+        left_node: Node<NumberNode<'a>>,
+        op_token: Token<'a>,
+        right_node: Node<NumberNode<'a>>,
+    ) -> Self {
+        Self {
+            left_node,
+            op_token,
+            right_node,
+        }
+    }
+}
+
+pub struct UnOpNode<'a> {
+    op_token: Token<'a>,
+    node: Node<NumberNode<'a>>,
+}
+
+impl<'a> UnOpNode<'a> {
+    pub fn new(op_token: Token<'a>, node: Node<NumberNode<'a>>) -> Self {
+        Self { op_token, node }
+    }
 }
