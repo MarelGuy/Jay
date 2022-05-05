8e5e65c570095931ad16e479219bb27b93f094ff
AverageHikikomori <golans.gaming@gmail.com>
2022-05-05 13:06:14 +0200

    added let/var/const nodes

---
+++ src/parser/ast/declarations.rs
@@ -0,0 +1,69 @@
+use crate::lexer::token::Token;
+
+use super::Node::Node;
+
+#[derive(Debug)]
+pub enum VarType {
+    Int,
+    Float,
+    Bool,
+    String,
+    Char,
+    Error,
+}
+
+#[derive(Debug)]
+pub enum AssignType {
+    Assign,
+    AddAssign,
+    SubAssign,
+    MulAssign,
+    DivAssign,
+    ModAssign,
+    PowAssign,
+    Error,
+}
+
+pub struct VarDeclNode {
+    name: String,
+    ty: VarType,
+    assign_op: AssignType,
+    mutable: bool,
+    value: String, // Values are stored in string just for debugging purposes, i'll add a prover value handling when the parser is done
+}
+
+impl VarDeclNode {
+    pub fn new(
+        name: String,
+        ty: VarType,
+        assign_op: AssignType,
+        mutable: bool,
+        value: String,
+    ) -> Self {
+        Self {
+            name,
+            ty,
+            assign_op,
+            mutable,
+            value,
+        }
+    }
+}
+
+pub struct ConstDeclNode {
+    name: String,
+    ty: VarType,
+    assign_op: AssignType,
+    value: String, // Values are stored in string just for debugging purposes, i'll add a prover value handling when the parser is done
+}
+
+impl ConstDeclNode {
+    pub fn new(name: String, ty: VarType, assign_op: AssignType, value: String) -> Self {
+        Self {
+            name,
+            ty,
+            assign_op,
+            value,
+        }
+    }
+}
