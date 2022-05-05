a6f5ca9f2a356886ff7715d8f16f8028fee34d74
AverageHikikomori <golans.gaming@gmail.com>
2022-05-05 13:05:57 +0200

    added better debugging, and var/let/const parsing

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -3,7 +3,9 @@ Jay parser
 Copyright (C) 2022  Loris Cuntreri
 */
 use crate::lexer::token::{Span, Token, TokenType};
+use crate::parser::ast::declarations::AssignType;
 
+use super::ast::declarations::{VarDeclNode, VarType};
 use super::ast::math_ops::{BinOpNode, UnOpNode};
 use super::ast::types::NumberNode;
 use super::ast::Node::Node;
@@ -64,14 +66,26 @@ impl<'a> Parser<'a> {
                         self.parse_number();
                     }
                 }
+                TokenType::Let => {
+                    self.parse_var(false, false);
+                    // useless comment, yay!
+                }
+                TokenType::Var => {
+                    self.parse_var(true, false);
+                    // another useless comment, even more yay!
+                }
+                TokenType::Const => {
+                    self.parse_var(false, true);
+                }
                 _ => self.next(),
-            }
+            };
         }
     }
 
     fn parse_number(&self) -> Node<NumberNode<'a>> {
-        println!("{:?}", self.current_token);
+        println!("num: {:?}", self.current_token.slice);
         let token: Token = self.current_token.clone();
+
         Node::new(vec![], NumberNode::new(token))
     }
 
@@ -80,7 +94,7 @@ impl<'a> Parser<'a> {
         self.next();
 
         let op_token: Token = self.current_token;
-        println!("{:?}", self.current_token);
+        println!("binop_type: {:?}", self.current_token.token_type);
         self.next();
 
         let right_node: Node<NumberNode> = self.parse_number();
@@ -93,8 +107,50 @@ impl<'a> Parser<'a> {
         self.next();
 
         let op_token: Token = self.current_token;
-        println!("{:?}", self.current_token);
+        println!("unop_type: {:?}", self.current_token.token_type);
 
         Node::new(vec![], UnOpNode::new(op_token, number_node))
+    }
+
+    fn parse_var(&mut self, is_mut: bool, is_const: bool) -> Node<VarDeclNode> {
+        self.next();
+        let name: String = self.current_token.slice.into();
+        println!("name: {:?}", name);
+
+        self.next();
+        self.next();
+
+        let ty: VarType = match self.current_token.token_type {
+            TokenType::IntType => VarType::Int,
+            TokenType::FloatType => VarType::Float,
+            TokenType::BoolType => VarType::Bool,
+            TokenType::StringType => VarType::String,
+            TokenType::CharType => VarType::Char,
+            _ => VarType::Error,
+        };
+        println!("type: {:?}", ty);
+
+        self.next();
+        let assign_token: AssignType = match self.current_token.token_type {
+            TokenType::Assign => AssignType::Assign,
+            TokenType::PlusAssign => AssignType::AddAssign,
+            TokenType::MinusAssign => AssignType::SubAssign,
+            TokenType::MultiplyAssign => AssignType::MulAssign,
+            TokenType::DivideAssign => AssignType::DivAssign,
+            TokenType::ModuloAssign => AssignType::ModAssign,
+            TokenType::PowerAssign => AssignType::PowAssign,
+            _ => AssignType::Error,
+        };
+        println!("assign_type: {:?}", assign_token);
+
+        self.next();
+
+        let value: String = self.current_token.slice.into();
+        println!("value: {:?}", value);
+
+        Node::new(
+            vec![],
+            VarDeclNode::new(name, ty, assign_token, is_mut, value),
+        )
     }
 }
