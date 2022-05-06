98a8be26ebc34aca18227e8f34140f26352cafbc
MarelGuy <golans.gaming@gmail.com>
2022-05-06 12:54:11 +0200

    moved Node to general, moved imports

--- src/parser/ast/math_ops.rs
+++ src/parser/ast/math_ops.rs
@@ -1,6 +1,6 @@
 use crate::lexer::token::Token;
 
-use super::{node::Node, types::NumberNode};
+use super::{general::Node, types::NumberNode};
 
 pub struct BinOpNode<'a> {
     left_node: Node<NumberNode<'a>>,
--- src/parser/ast/mod.rs
+++ src/parser/ast/mod.rs
@@ -1,15 +1,5 @@
-pub(crate) mod node {
-    pub struct Node<T> {
-        children: Vec<T>,
-        node: T,
-    }
-
-    impl<T> Node<T> {
-        pub fn new(children: Vec<T>, node: T) -> Self {
-            Self { children, node }
-        }
-    }
-}
 pub(crate) mod declarations;
+pub(crate) mod general;
+pub(crate) mod if_else;
 pub(crate) mod math_ops;
 pub(crate) mod types;
