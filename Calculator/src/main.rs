f31a534149f4e51dfaf7442769a93bbba66020d8
Marelguy <golans.gaming@gmail.com>
2022-05-11 22:07:38 +0200

    added lexing

--- Calculator/src/main.rs
+++ Calculator/src/main.rs
@@ -1,3 +1,40 @@
+mod lexer;
+
+use std::{env::args, fs::read_to_string, path::Path};
+
+use lexer::{
+    lexer::Lexer,
+    token::{Token, TokenType},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+
+    if args.len() < 2 {
+        println!("Error: No file specified");
+        return;
+    }
+
+    let file_path: &Path = Path::new(&args[1]);
+
+    if !file_path.exists() {
+        println!("Error: file does not exist");
+        return;
+    }
+
+    let file_content: String = read_to_string(file_path).expect("Error: failed to read file");
+
+    let lexer: Lexer = Lexer::new(&file_content);
+
+    let mut tokens: Vec<Token> = Vec::new();
+
+    for token in lexer {
+        if token.token_type != TokenType::Space {
+            tokens.push(token);
+        }
+    }
+
+    for token in tokens {
+        println!("{:?}", token);
+    }
 }
