b9c59857192369971d017e91819b0813f6edb605
Marelguy <golans.gaming@gmail.com>
2022-05-23 07:43:38 +0200

    moved TypeNode and added CharNode & StringNode

--- src/parser/ast/types.rs
+++ src/parser/ast/types.rs
@@ -1,5 +1,19 @@
 use crate::lexer::token::Token;
 
+use super::general::Node;
+
+#[derive(PartialEq, Debug)]
+pub struct TypeNode<'a> {
+    name: String,
+    fields: Vec<Box<Node<'a>>>,
+}
+
+impl<'a> TypeNode<'a> {
+    pub fn new(name: String, fields: Vec<Box<Node<'a>>>) -> Self {
+        Self { name, fields }
+    }
+}
+
 #[derive(PartialEq, Debug)]
 pub struct NumberNode<'a> {
     token: Token<'a>,
@@ -8,5 +22,28 @@ pub struct NumberNode<'a> {
 impl<'a> NumberNode<'a> {
     pub fn new(token: Token<'a>) -> Self {
         Self { token }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub struct CharNode<'a> {
+    token: Token<'a>,
+}
+
+impl<'a> CharNode<'a> {
+    pub fn new(token: Token<'a>) -> Self {
+        Self { token }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub struct StringNode<'a> {
+    token: Token<'a>,
+    length: usize,
+}
+
+impl<'a> StringNode<'a> {
+    pub fn new(token: Token<'a>, length: usize) -> Self {
+        Self { token, length }
     }
 }
