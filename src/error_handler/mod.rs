c1f89ae33a168a68a782e2a0539c19082578ed9f
Marelguy <golans.gaming@gmail.com>
2022-08-17 13:52:16 +0200

    added two error types

--- src/error_handler/mod.rs
+++ src/error_handler/mod.rs
@@ -1,15 +1,64 @@
-struct Error {
-    message: String,
+use colored::Colorize;
+use std::process::exit;
+
+pub struct Error<'a> {
     line: usize,
+    line_string: String,
+    slice: &'a str,
     column: usize,
+    file_name: String,
 }
 
-impl Error {
-    fn new(message: String, line: usize, column: usize) -> Self {
+impl<'a> Error<'a> {
+    pub fn new(
+        line: usize,
+        line_string: String,
+        slice: &'a str,
+        column: usize,
+        file_name: String,
+    ) -> Self {
         Self {
-            message,
             line,
+            line_string,
+            slice,
             column,
+            file_name,
         }
+    }
+
+    pub fn print(&self) {
+        let error_counter: String = "^".repeat(self.slice.len());
+        println!("{} file: {}", "-".blue(), self.file_name);
+        println!("{}", "|".blue());
+        println!("{} {}.   {}", "|".blue(), self.line, self.line_string);
+        println!(
+            "{}      {}{}",
+            "|".blue(),
+            " ".repeat(self.column),
+            error_counter.yellow()
+        );
+        println!("{}", "-".blue());
+    }
+
+    pub fn throw_var_not_defined(&self, var_name: &str) {
+        println!(
+            "{}: cannot find variable {} in this scope",
+            "error".red(),
+            var_name
+        );
+        self.print();
+        exit(0)
+    }
+
+    pub fn throw_wrong_assign_type(&self, var_name: &str, val_type: String, var_type: String) {
+        println!(
+            "{}: cannot assign value of type {} to variable {}: {}",
+            "error".red(),
+            val_type,
+            var_name,
+            var_type
+        );
+        self.print();
+        exit(0)
     }
 }
